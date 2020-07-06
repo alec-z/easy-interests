f8cb1db288f13a98e79cf70a75c349e6803b9732
alec.zheng <aleczheng05@gmail.com>
2020-07-06 12:10:35 +0800

    change remaining front2->front

--- interests-front/e2e/src/app.e2e-spec.ts
+++ interests-front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('interests-front2 app is running!');
+    expect(page.getTitleText()).toEqual('interests-front app is running!');
   });
 
   afterEach(async () => {

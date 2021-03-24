d190e1e4849e4132bcf3ddeb738a34697b9192dd
alec.zheng <zhengzhipeng4@huawei.com>
2021-03-24 17:55:50 +0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-SHA=$(git rev-parse HEAD)
-docker build -t aleczheng/interests-front:lastest -t aleczheng/interests-front:$SHA -f ./interests-front/Dockerfile ./interests-front
-docker push aleczheng/interests-front:lastest
+docker build -t aleczheng/interests-front:latest -t aleczheng/interests-front:$SHA -f ./interests-front/Dockerfile ./interests-front
+docker push aleczheng/interests-front:latest
 docker push aleczheng/interests-front:$SHA
 
 docker build -t aleczheng/interests-back:lastest -t aleczheng/interests-back:$SHA -f ./interests-back/Dockerfile ./interests-back
-docker push aleczheng/interests-back:lastest
-docker push aleczheng/interests-front:$SHA
+docker push aleczheng/interests-back:latest
+docker push aleczheng/interests-back:$SHA
 
 kubectl apply -f google_k8s
 

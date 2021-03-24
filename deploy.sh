766000dfc5a3e1a3e3314fa591ad75f35b0d4ac1
alec.zheng <zhengzhipeng4@huawei.com>
2021-03-24 09:31:09 +0800

    bug fix

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+docker build -t aleczheng/interests-front:lastest -t aleczheng/interests-front:$SHA -f ./interests-front/Dockerfile ./interests-front
+docker push aleczheng/interests-front:lastest
+docker push aleczheng/interests-front:$SHA
+
+docker build -t aleczheng/interests-back:lastest -t aleczheng/interests-back:$SHA -f ./interests-back/Dockerfile ./interests-back
+docker push aleczheng/interests-back:lastest
+docker push aleczheng/interests-front:$SHA
+
+kubectl apply -f google_k8s
--- interests-back/main.go
+++ interests-back/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"github.com/alec-z/interests-back/api"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
 func main() {
 	RouterInit()
+}
+
+func RouterInit() {
+	// New Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.POST("/api/cal", api.Calculate)
+	e.GET("/api/ws_cal", api.WsCalculate)
+
+	e.POST("/api/cal_item", api.CalculateItem)
+
+	// Start server
+	e.Logger.Info(e.Start(":3000"))
 }

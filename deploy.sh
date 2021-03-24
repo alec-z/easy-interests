217d8af5e38f1dc71c4eb3e0fa9676e1904ee577
alec.zheng <zhengzhipeng4@huawei.com>
2021-03-24 18:08:32 +0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
-docker build -t aleczheng/interests-front:latest -t aleczheng/interests-front:$SHA -f ./interests-front/Dockerfile ./interests-front
+SHA=$(git rev-parse HEAD)
+docker build -t aleczheng/interests-front -t aleczheng/interests-front:$SHA -f ./interests-front/Dockerfile ./interests-front
+docker build -t aleczheng/interests-back -t aleczheng/interests-back:$SHA -f ./interests-back/Dockerfile ./interests-back
+
 docker push aleczheng/interests-front:latest
 docker push aleczheng/interests-front:$SHA
 
-docker build -t aleczheng/interests-back:lastest -t aleczheng/interests-back:$SHA -f ./interests-back/Dockerfile ./interests-back
 docker push aleczheng/interests-back:latest
 docker push aleczheng/interests-back:$SHA
 
